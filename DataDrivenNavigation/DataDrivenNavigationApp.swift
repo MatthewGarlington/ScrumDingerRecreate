e7b042b8dfa61132b03bb28476004bc65dbb78bf
Matthew Garlington <61671107+MatthewGarlington@users.noreply.github.com>
2022-12-14 10:39:34 -0500

    Initial Commit

---
+++ DataDrivenNavigation/DataDrivenNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataDrivenNavigationApp.swift
+//  DataDrivenNavigation
+//
+//  Created by Matthew Garlington on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct DataDrivenNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
